c4d7459137dc3502bb32bc16c8cf2cc2c3f385e9
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2021-11-19 15:32:56 +0100

    Update, now only problem with object in functions passing

--- index.js
+++ index.js
@@ -3,36 +3,68 @@
 // Input: a (number), b (number)
 // Action: Add both numbers together
 // Output: The result (number)
-function add() {
-  // write your code here
+function add(a, b) {
+
+  let sum = a + b
+  console.log(`The sum is : ${Number(sum)}`)
+  return sum
+
 }
 
 // Input: number
 // Action: Check if the number given is even or not
 // Output: The result (boolean)
-function isEven() {
-  // write your code here
+function isEven(number) {
+
+  if (number % 2 === 0) {
+    console.log(`It is Even : ${Boolean(number)}`)
+    return Boolean(number)
+  }
+
+  else if (number % 2 === 1) {
+    console.log(`It is odd : ${Boolean(number)}`)
+    return Boolean(number)
+  }
+
 }
 
 // Input: name (string)
 // Action: Greet the user by name
 // Output: The greeting (string)
-function greet() {
-  // write your code here
+function greet(name) {
+
+    alert(`Hi user : ${name}`)
+    return name
+
 }
 
 // Input: age (number)
 // Action: Check if the age is 18 or over
 // Output: The result (boolean)
-function isAnAdult() {
-  // write your code here
+function isAnAdult(ageOfPersonParameter) {
+
+    if (Number(ageOfPersonParameter) >= 18) {
+        alert("It is adult")
+        return true
+    }
+
+    else {
+        alert("You are a kid !")
+        return false
+    }
+
 }
 
 // Input: age (number)
 // Action: Check how many years are left until adulthood (18)
 // Output: The result (number)
-function yearsToAdulthood() {
-  // write your code here
+function yearsToAdulthood(numberParameter) {
+
+    if (numberParameter < 18) {
+        alert(`You are ${(18 - numberParameter)} years away of adult age`)
+        return (18 - numberParameter)
+    }
+
 }
 
 // Input: person ({ age: number, name: string })
@@ -41,7 +73,48 @@ function yearsToAdulthood() {
 //   - if they are, greet them
 //   - if they are not, tell them to come back in X years (when they are)
 // Output: The result (string)
-function admit() {
-  // write your code here
-  // use greet, isAnAdult and yearsToAdulthood to help you!
+function admit(personParameter = {
+    ageParameter: null, 
+    nameParameter: null
+}) {
+
+    alert("Here we check everything")
+    let insideGreet = greet(personParameter.nameParameter)
+    let insideAdult = isAnAdult(personParameter.ageParameter)
+    let insideAdultHood = yearsToAdulthood(personParameter.ageParameter)
+
+    if (insideAdult) {
+        alert(`Hi : ${insideGreet}`)
+    }
+
+    else {
+        alert(`Hi : ${insideGreet} you can come after : ${insideAdultHood} years when you will become an adult`)
+    }
+
 }
+
+let nameOfPerson = prompt("Please enter your name: ")
+let ageOfPerson = Number(prompt("Please enter your age: "))
+let numberOfPerson = Number(prompt("Enter a number to check"))
+
+let person = {
+    age: ageOfPerson,
+    name: nameOfPerson
+}
+console.log(`Object value age: ${person.age} and name: ${person.name}`)
+
+let fuctionSum = add(numberOfPerson, 10)
+let functionIsEven = isEven(numberOfPerson)
+let functionGreet = greet(nameOfPerson)
+let functionIsAdult = isAnAdult(ageOfPerson)
+let functionAdultHood = yearsToAdulthood(ageOfPerson)
+
+console.log("Sum is: " + fuctionSum)
+console.log(`Hi ${functionGreet} you are Adult: ${functionIsAdult} \n 
+and if you are not then you are : ${functionAdultHood} years from becoming one adult \n, 
+\n The number you entered : ${numberOfPerson}`)
+
+admit(person = {
+    age: ageOfPerson,
+    name: nameOfPerson
+})
